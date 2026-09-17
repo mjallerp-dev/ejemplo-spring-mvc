09657e52142d0d1a5317135e0fc9453703725845
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-16 21:11:37 -0500

    chore: initial Spring Boot MVC project setup

---
+++ src/test/java/mintic/misiontic/ciclo3/EjemploSpring/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3.EjemploSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
