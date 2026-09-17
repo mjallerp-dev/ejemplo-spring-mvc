cfbdcf4b9b7cfce2a04b67bc987559d81a1eacb8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-16 21:15:02 -0500

    feat(usuario): add Usuario model class

---
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/modelo/Usuario.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3.EjemploSpring.modelo;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+
+    private String cedula;
+    private String clave;
+    private String nombre;
+    private String email;
+
+}
