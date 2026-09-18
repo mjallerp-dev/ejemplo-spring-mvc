e68623430ac0122d415847407039b4123b4abf9e
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-17 20:07:03 -0500

    feat(usuario): map Usuario as JPA entity
    This reverts commit 3943b99b6b4c4f5d0d118d75e50eb4f5361782d9.

--- src/main/java/mintic/misiontic/ciclo3/EjemploSpring/modelo/Usuario.java
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/modelo/Usuario.java
@@ -1,10 +1,19 @@
 package mintic.misiontic.ciclo3.EjemploSpring.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-@Data
-public class Usuario {
+import java.io.Serializable;
 
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
     private String cedula;
     private String clave;
     private String nombre;
