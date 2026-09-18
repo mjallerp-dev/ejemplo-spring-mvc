40b4b68f7afe0a0dce5a0bffa326951380c70343
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-17 20:22:46 -0500

    feat(usuario): add IUsuarioServicio interface

---
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/servicio/IUsuarioServicio.java
@@ -0,0 +1,15 @@
+package mintic.misiontic.ciclo3.EjemploSpring.servicio;
+
+import mintic.misiontic.ciclo3.EjemploSpring.modelo.Usuario;
+import java.util.List;
+
+public interface IUsuarioServicio {
+
+    public List<Usuario> listarUsuarios();
+
+    public void guardar(Usuario user);
+
+    public void eliminar(Usuario user);
+
+    public Usuario buscar(Usuario user);
+}
