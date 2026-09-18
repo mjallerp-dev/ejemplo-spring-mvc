a2682a45d5d562ab50f998d7084e94ee16ef14de
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-18 02:11:32 -0500

    feat(articulo): add ArticuloServicioImp implementation

---
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/servicio/ArticuloServicioImp.java
@@ -0,0 +1,40 @@
+package mintic.misiontic.ciclo3.EjemploSpring.servicio;
+
+import mintic.misiontic.ciclo3.EjemploSpring.dao.IArticuloCrud;
+import mintic.misiontic.ciclo3.EjemploSpring.modelo.Articulo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ArticuloServicioImp implements IArticuloServicio {
+
+    @Autowired
+    IArticuloCrud crudArticulo;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Articulo> listarArticulos() {
+        return (List<Articulo>) crudArticulo.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void guardar(Articulo articulo) {
+        crudArticulo.save(articulo);
+    }
+
+    @Transactional
+    @Override
+    public void eliminar(Articulo articulo) {
+        crudArticulo.delete(articulo);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Articulo buscar(Articulo articulo) {
+        return crudArticulo.findById(articulo.getId()).orElse(null);
+    }
+}
