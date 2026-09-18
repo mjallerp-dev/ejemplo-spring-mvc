9a99dd5c8a62eddffbe319ffe40f3f86879376e2
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-09-17 21:07:09 -0500

    refactor(inicio): use IUsuarioServicio in ControladorInicio

--- src/main/java/mintic/misiontic/ciclo3/EjemploSpring/controladores/ControladorInicio.java
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/controladores/ControladorInicio.java
@@ -1,8 +1,8 @@
 package mintic.misiontic.ciclo3.EjemploSpring.controladores;
 
 import lombok.extern.slf4j.Slf4j;
-import mintic.misiontic.ciclo3.EjemploSpring.dao.IUsuarioCrud;
 import mintic.misiontic.ciclo3.EjemploSpring.modelo.Usuario;
+import mintic.misiontic.ciclo3.EjemploSpring.servicio.IUsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +15,13 @@ import java.util.List;
 public class ControladorInicio {
 
     @Autowired
-    IUsuarioCrud crudUsuario;
+
+    IUsuarioServicio userServicio;
+
     @GetMapping("/")
     public String inicio(Model modelo) {
-        List<Usuario> listaUsuarios = (List<Usuario>) crudUsuario.findAll();
+        //List<Usuario> listaUsuarios =
+        List<Usuario> listaUsuarios = (List<Usuario>) userServicio.listarUsuarios();
         modelo.addAttribute("usuarios", listaUsuarios);
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
